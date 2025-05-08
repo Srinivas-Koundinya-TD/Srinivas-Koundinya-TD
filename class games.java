37df34bb068f542258ac28cd9a80c8f8ce65fc86
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-08 23:42:10 +0530

    Add files via upload

---
+++ class games.java
@@ -0,0 +1,24 @@
+class games
+public static void main(string [] args)
+{
+     int ch;
+    
+    (if ch == "1"){
+    System.out.println(" you can play outdoor games");
+    System.out.println(" you can play cricket");
+}
+    else{
+
+    System.out.println(" you can play badminton");
+}
+    (if ch == "2"){
+    System.out.println(" you can play indoor games");
+    System.out.println(" you can play chess");
+}
+    else{
+
+    System.out.println(" you can play carrom");
+}
+    
+}
+    

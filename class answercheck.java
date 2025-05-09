9715ae824c3195105f23813eba303775ea300dee
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-10 00:18:40 +0530

    simple answer checking

---
+++ class answercheck.java
@@ -0,0 +1,13 @@
+class answercheck
+public static void main (string[]args)
+{
+    
+    int i ;
+
+    if(int i==1)
+    {
+    System.out.println("answer is correct");
+    }
+    else
+    System.out.println("answer is incorrect");
+}

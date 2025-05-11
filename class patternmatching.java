0477179ecfe075c0e0e5274dc59cc8ea357ca309
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-11 23:17:55 +0530

    simple pattern printing

---
+++ class patternmatching.java
@@ -0,0 +1,8 @@
+class patternmatching
+public static void main(string[]args)
+{
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
+}

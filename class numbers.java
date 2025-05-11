590db69dad8b8849793de0fc274a568b12a5534a
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-11 23:18:35 +0530

    printing from 1 to 10

---
+++ class numbers.java
@@ -0,0 +1,10 @@
+class numbers
+public static void main(string[]args)
+{
+    Scanner scanner = new Scanner(System.in);
+    int i;
+    for(i=0;i<10;i++){
+
+        System.out.println("printing the numbers from 1 to 10");
+    }
+}

546b2686ad1501bc71802c9d85e1aecb4700c3f8
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-12 22:14:46 +0530

    animal sounds printing

---
+++ class animalsounds.java
@@ -0,0 +1,8 @@
+class animalsounds
+public static void main(string[]args)
+{
+        System.out.println("cat sounds meow");
+        System.out.println("dog barks");
+         System.out.println("grasshopper chirps");
+
+}

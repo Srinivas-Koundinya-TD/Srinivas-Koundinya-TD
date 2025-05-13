b6a4f7f43406cf902ac87e77686dfd39a15247d9
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-13 23:52:13 +0530

    printing star pattern

---
+++ class pattern.java
@@ -0,0 +1,8 @@
+class pattern
+public static void main(string[]args)
+{
+     System.out.println("*");
+     System.out.println("**");
+     System.out.println("***");
+     System.out.println("****");
+}

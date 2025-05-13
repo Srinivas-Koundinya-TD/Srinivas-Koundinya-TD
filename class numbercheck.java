f3ad1873e60f47479d7627fd02e5dcb7b8e9eaa1
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-05-13 23:53:08 +0530

    Greater or lesser checking

---
+++ class numbercheck.java
@@ -0,0 +1,14 @@
+class numbercheck
+public static void main(string[]args)
+{
+    int n;
+
+    if(n>=10){
+    
+    System.out.println("n is greater than 10");
+    }
+
+    else
+        System.out.println("n is smaller than 10");
+}
+

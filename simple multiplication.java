93e3ea671eac99c4a039f86a6d4a3a0fe708dcb3
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-12 00:08:45 +0530

    Update simple multiplication.java

--- simple multiplication.java
+++ simple multiplication.java
@@ -1,10 +1,10 @@
-class mul;
-public static void main(string[]args);
+class Mul{
+public static void main(String[]args)
 {
-    int n1=2
-    int n2=3
-    int product
-
-    product=n1*n2;
-    system.out.println("product of two numbers");
+    int n1=2;
+    int n2=3;
+    int product=n1*n2;
+    
+    System.out.println(n1*n2);
+}
 }

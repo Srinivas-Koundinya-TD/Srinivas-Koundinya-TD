0e85297f7a042a7d4eb420926a2fb8904b8ecbec
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-13 22:33:38 +0530

    DataTypes.java
    printing eight primitive data types of java

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+class DataTypesMain{
+    public static void main(String[]args){
+        
+        byte b= 2;
+        System.out.println(b);
+        
+        int i= 57;
+        System.out.println(i);
+        
+        char ch = 'h';
+        System.out.println(ch);
+       
+        long l= 27856;
+        System.out.println(l);
+        
+        float val = 8.98645f;
+        System.out.println(b);
+        
+        String str= "rabbit";
+        System.out.println(str);
+        
+        double d= 453.9708;
+        System.out.println(d);
+        
+        short s= 8;
+        System.out.println(b);
+}
+}

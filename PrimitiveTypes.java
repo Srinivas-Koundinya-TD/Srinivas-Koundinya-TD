64f1295d30e6e29f682336d9048fdfa43b8ca1e2
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-14 23:18:44 +0530

    Add files via upload
    Taking input from scanner and printing the datatypes

---
+++ PrimitiveTypes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        
+        String input = sc.next();
+        System.out.println(input);
+
+        int e =  sc.nextInt();
+        System.out.println(e);
+        
+        char ch= sc.next().charAt(0);
+        System.out.println(ch);
+        
+        double d= sc.nextDouble();
+        System.out.println(d);
+        
+        float  f= sc.nextFloat();
+        System.out.println(f);
+        
+}
+}

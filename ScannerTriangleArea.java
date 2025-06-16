6175b42e6223e63f1fdfb71461ec7f61e81f6c6e
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-16 23:18:39 +0530

    Add files via upload
    Calculating area of equilateral triangle

---
+++ ScannerTriangleArea.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ScannerTriangleArea {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        
+        
+        int rad= sc.nextInt();
+        int area  =3*rad*rad;
+        System.out.println(area);
+        
+}
+}

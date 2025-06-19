a7e46cf5f5aa9609ad8757fad539935f15630479
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-19 23:41:11 +0530

    Add files via upload

---
+++ ScannerSquarePerimeter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ScannerSquarePerimeter {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        
+        
+        int rad= sc.nextInt();
+        int area  =4*rad;
+        System.out.println(area);
+        
+}
+}

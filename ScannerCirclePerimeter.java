b33b8885a38fb7fd0d584c9214212083832faeb0
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-06-20 23:28:21 +0530

    Add files via upload

---
+++ ScannerCirclePerimeter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ScannerCirclePerimeter {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner (System.in);
+        
+        
+        int rad= sc.nextInt();
+        float area = 2 * 3.14f * rad;
+        System.out.println(area);
+        
+}
+}

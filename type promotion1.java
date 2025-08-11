7394e106b5d460acd7f5423c2347ff8c8f7df9a0
Srinivas-Koundinya-TD <srinidhi.koundinya005@gmail.com>
2025-08-11 22:27:10 +0530

    Add files via upload

---
+++ type promotion1.java
@@ -0,0 +1,12 @@
+
+import java.util.*;
+public class JavaBasics {
+    public static void main(String[] args) {
+        
+        char a= 25;
+        char b=89;
+        System.out.println((int)(b));
+        System.out.println((int)(a));
+        System.out.println(b-a);
+    }
+}
